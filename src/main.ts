93bf2ba007c4705c158ce3aceea312fa9bd38c7a
Bobby R. Ward <bobbyrward@gmail.com>
2019-11-19 06:32:04 -0600

    Add use_package_version input

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core';
-import {wait} from './wait'
+import * as exec from '@actions/exec';
 import * as requests from 'request-promise-native';
 
 async function run() {
@@ -8,7 +8,28 @@ async function run() {
     const pypi_username: string = core.getInput("pypi_username");
     const pypi_password: string = core.getInput("pypi_password");
     const package_name: string = core.getInput("package_name");
-    const version: string = core.getInput("version");
+
+    const use_package_version: string = core.getInput("use_package_version");
+    var version: string = core.getInput("version");
+
+
+    if (use_package_version == "true") {
+      var output: string = "";
+
+      await exec.exec('python', ['setup.py', '--version'], {
+        listeners: {
+          stdout: (data: Buffer) => {
+            output += data.toString();
+          }
+        }
+      });
+
+      version = output;
+    }
+
+    if (!version) {
+        throw new Error("Must specify version or use_package_version");
+    }
 
     const resp = await requests.get(
       `https://${pypi_hostname}/api/package/dpn_events_python/`,
@@ -20,8 +41,7 @@ async function run() {
 
     for (var existing_package of resp.packages) {
       if (existing_package.version == version) {
-        core.setFailed("Version already exists in package server");
-        break
+        throw new Error("Version already exists in package server");
       }
     }
   } catch (error) {
