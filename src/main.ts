84e0c9da7f252791b616375b227f7a79b549c231
Bobby R. Ward <bobbyrward@gmail.com>
2019-11-19 05:35:43 -0600

    Conflict check action

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,29 @@
 import * as core from '@actions/core';
 import {wait} from './wait'
+import * as requests from 'request-promise-native';
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    const pypi_hostname: string = core.getInput("pypi_hostname");
+    const pypi_username: string = core.getInput("pypi_username");
+    const pypi_password: string = core.getInput("pypi_password");
+    const package_name: string = core.getInput("package_name");
+    const version: string = core.getInput("version");
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
+    const resp = await requests.get(
+      `https://${pypi_hostname}/api/package/dpn_events_python/`,
+      {
+        auth: {user: pypi_username, password: pypi_password},
+        json: true
+      }
+    );
 
-    core.setOutput('time', new Date().toTimeString());
+    for (var existing_package of resp.packages) {
+      if (existing_package.version == version) {
+        core.setFailed("Version already exists in package server");
+        break
+      }
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
